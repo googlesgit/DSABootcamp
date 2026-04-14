0d2bf60140c47816c95f62843e48b11e89336942
googlesgit <miyapuramsairamreddy@gmail.com>
2026-04-14 13:08:13 -0500

    Created folder structure

---
+++ src/flowofprogram/Main.java
@@ -0,0 +1,7 @@
+package src.flowofprogram;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello Sai, lets deal with DSA and get on it");
+    }
+}
---
+++ src/flowofprogram/Primitives.java
@@ -0,0 +1,21 @@
+package src.flowofprogram;
+
+public class Primitives {
+    public static void main(String[] args) {
+        int rollno = 64;
+        String name = "Sai";
+        float marks = 98.67f;
+        char letter = 's';
+        double largeDecimalNumbers = 456756.76543;
+        long largeIntegerNumbers = 45675676543L;
+        boolean check = false;
+
+        System.out.println(rollno);
+        System.out.println(name);
+        System.out.println(marks);
+        System.out.println(letter);
+        System.out.println(largeDecimalNumbers);
+        System.out.println(largeIntegerNumbers);
+        System.out.println(check);
+    }
+}
